7c8b027d808cc017d9c72764919d6a6d2a43e988
warmacin <Shaniyer98@gmail.com>
2026-07-28 12:18:58 +0530

    Completed milestone 2 networking

--- terraform/shared-services/networking/main.tf
+++ terraform/shared-services/networking/main.tf
@@ -1,11 +1,9 @@
 module "shared_services_vpc" {
+
   source = "../../modules/vpc"
 
   vpc_name = var.vpc_name
   vpc_cidr = var.vpc_cidr
 
-  tags = {
-    Project     = "Crystal"
-    Environment = "SharedServices"
-  }
+  tags = var.common_tags
 }
