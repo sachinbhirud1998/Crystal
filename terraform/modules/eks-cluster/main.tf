cee3f314b09d8d388d2fbed65b204401fd805cff
warmacin <Shaniyer98@gmail.com>
2026-07-30 18:11:51 +0530

    Cluster is Up

---
+++ terraform/modules/eks-cluster/main.tf
@@ -0,0 +1,56 @@
+############################################################
+# Project Crystal
+# Module      : EKS Cluster
+############################################################
+
+resource "aws_eks_cluster" "this" {
+
+  ##########################################################
+  # Cluster Configuration
+  ##########################################################
+
+  name    = var.cluster_name
+
+  version = var.kubernetes_version
+
+  role_arn = var.cluster_role_arn
+
+  ##########################################################
+  # Networking
+  ##########################################################
+
+  vpc_config {
+
+    subnet_ids = var.private_subnet_ids
+
+    endpoint_private_access = var.endpoint_private_access
+
+    endpoint_public_access = var.endpoint_public_access
+
+    public_access_cidrs = var.public_access_cidrs
+
+  }
+
+  ##########################################################
+  # Control Plane Logging
+  ##########################################################
+
+  enabled_cluster_log_types = var.enabled_cluster_log_types
+
+  ##########################################################
+  # Tags
+  ##########################################################
+
+  tags = merge(
+
+    var.tags,
+
+    {
+
+      Name = var.cluster_name
+
+    }
+
+  )
+
+}
---
+++ terraform/production/eks-cluster/main.tf
@@ -0,0 +1,129 @@
+############################################################
+# Project Crystal
+# Production EKS Cluster
+############################################################
+
+############################################################
+# Production Networking Remote State
+############################################################
+
+data "terraform_remote_state" "networking" {
+
+  backend = "local"
+
+  config = {
+
+    path = "../networking/terraform.tfstate"
+
+  }
+
+}
+
+############################################################
+# EKS Control Plane IAM Role
+############################################################
+
+module "eks_cluster_iam_role" {
+
+  source = "../../modules/iam-role"
+
+  role_name = var.cluster_role_name
+
+  role_description = "IAM Role for Project Crystal Production EKS Control Plane"
+
+  service_principal = "eks.amazonaws.com"
+
+  managed_policy_arns = [
+
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+
+    "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+
+  ]
+
+  tags = merge(
+
+    var.common_tags,
+
+    {
+
+      Name = var.cluster_role_name
+
+    }
+
+  )
+
+}
+
+############################################################
+# Local Values
+############################################################
+
+locals {
+
+  private_subnet_ids = [
+
+    for subnet_name in var.private_subnet_names :
+
+    data.terraform_remote_state.networking.outputs.private_subnet_ids[subnet_name]
+
+  ]
+
+}
+############################################################
+# Amazon EKS Cluster
+############################################################
+
+module "production_eks_cluster" {
+
+  source = "../../modules/eks-cluster"
+
+  ##########################################################
+  # Cluster Configuration
+  ##########################################################
+
+  cluster_name = var.cluster_name
+
+  kubernetes_version = var.kubernetes_version
+
+  ##########################################################
+  # IAM
+  ##########################################################
+
+  cluster_role_arn = module.eks_cluster_iam_role.role_arn
+
+  ##########################################################
+  # Networking
+  ##########################################################
+
+  private_subnet_ids = local.private_subnet_ids
+
+  endpoint_private_access = var.endpoint_private_access
+
+  endpoint_public_access = var.endpoint_public_access
+
+  public_access_cidrs = var.public_access_cidrs
+
+  ##########################################################
+  # Logging
+  ##########################################################
+
+  enabled_cluster_log_types = var.enabled_cluster_log_types
+
+  ##########################################################
+  # Tags
+  ##########################################################
+
+  tags = var.common_tags
+
+  ##########################################################
+  # Dependencies
+  ##########################################################
+
+  depends_on = [
+
+    module.eks_cluster_iam_role
+
+  ]
+
+}
