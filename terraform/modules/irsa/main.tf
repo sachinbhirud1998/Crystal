ea5716deeb67b2d9b7cbee448c1a6ee47ffd6b5e
Sachin <sachinbhirud1998@gmail.com>
2026-08-02 12:46:19 +0530

    irsa

--- terraform/modules/irsa/main.tf
+++ terraform/modules/irsa/main.tf
@@ -1,1 +1,104 @@
+############################################################
+# Project Crystal
+# IRSA Module
+############################################################
 
+############################################################
+# Trust Policy
+############################################################
+
+data "aws_iam_policy_document" "assume_role" {
+
+  statement {
+
+    effect = "Allow"
+
+    actions = [
+
+      "sts:AssumeRoleWithWebIdentity"
+
+    ]
+
+    principals {
+
+      type = "Federated"
+
+      identifiers = [
+
+        var.oidc_provider_arn
+
+      ]
+
+    }
+
+    condition {
+
+      test = "StringEquals"
+
+      variable = "${replace(var.oidc_provider_url, "https://", "")}:sub"
+
+      values = [
+
+        "system:serviceaccount:${var.namespace}:${var.service_account_name}"
+
+      ]
+
+    }
+
+    condition {
+
+      test = "StringEquals"
+
+      variable = "${replace(var.oidc_provider_url, "https://", "")}:aud"
+
+      values = [
+
+        "sts.amazonaws.com"
+
+      ]
+
+    }
+
+  }
+
+}
+
+############################################################
+# IAM Role
+############################################################
+
+resource "aws_iam_role" "this" {
+
+  name = var.role_name
+
+  description = var.role_description
+
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+
+  tags = merge(
+
+    var.tags,
+
+    {
+
+      Name = var.role_name
+
+    }
+
+  )
+
+}
+
+############################################################
+# Managed Policies
+############################################################
+
+resource "aws_iam_role_policy_attachment" "managed" {
+
+  for_each = toset(var.managed_policy_arns)
+
+  role = aws_iam_role.this.name
+
+  policy_arn = each.value
+
+}
--- terraform/modules/irsa/outputs.tf
+++ terraform/modules/irsa/outputs.tf
@@ -1,1 +1,21 @@
 
+############################################################
+# Project Crystal
+# IRSA Module
+############################################################
+
+output "role_name" {
+
+  description = "IAM Role Name"
+
+  value = aws_iam_role.this.name
+
+}
+
+output "role_arn" {
+
+  description = "IAM Role ARN"
+
+  value = aws_iam_role.this.arn
+
+}
